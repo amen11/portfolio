511e3be520f911474c5fb33f94d7b160dded442c
Amen Korchane <41312751+amen11@users.noreply.github.com>
2023-08-09 18:58:35 +0100

    added backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import About from "@/components/About";
 import ContactMe from "@/components/ContactMe";
 import Header from "@/components/Header";
@@ -6,6 +7,7 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -43,7 +45,13 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
-
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" src="" alt="logo" />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '1owf8q2g',
+    dataset: 'production'
+  }
+})
