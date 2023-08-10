e07af8f6d7460de20482bcac7174936730e5796e
Amen Korchane <41312751+amen11@users.noreply.github.com>
2023-08-10 16:00:27 +0100

    connecting backend

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ function Header({}: Props) {
           bgColor="transparent"
         />
       </motion.div>
-      <Link href="#contact">
+      <a href="#contact">
         <motion.div
           initial={{
             x: 500,
@@ -65,7 +65,7 @@ function Header({}: Props) {
             Get In Touch
           </p>
         </motion.div>
-      </Link>
+      </a>
     </header>
   );
 }
---
+++ pages/api/getSocials.ts
@@ -0,0 +1,5 @@
+ import type { NextApiRequest, NextApiResponse } from "next";
+ import { groq } from "next-sanity";
+ import { sanityClient } from "../../sanity";
+
+const query = groq ``
