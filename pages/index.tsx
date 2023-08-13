860b21f17871ec56721e713ed375c8b323e79285
Amen Korchane <41312751+amen11@users.noreply.github.com>
2023-08-13 22:16:41 +0100

    websitelogo

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,7 @@ const Home = ({pageInfo, experiences, skills, projects, socials} : Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
+        <link rel="shortcut icon" href="../public/amenlogorounded.png" />
         <title>Amen portfolio</title>
       </Head>
 
@@ -69,7 +70,6 @@ const Home = ({pageInfo, experiences, skills, projects, socials} : Props) => {
               className="w-10 h-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
               src={logo}
               alt="logo"
-              
             />
           </div>
         </footer>
