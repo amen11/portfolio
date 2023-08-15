c065940b635c4edf3d569e66b9d6058558017d88
Amen Korchane <41312751+amen11@users.noreply.github.com>
2023-08-15 21:40:21 +0100

    adedd analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
+  return (
     <>
-  <Head>
+      <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-   <Component {...pageProps} />; 
-
-</>
-)
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
   }
